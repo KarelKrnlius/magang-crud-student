e6409f854a482889877b0f022abcf4e08698c9d7
Kenzie-Felix <kenziemuara@gmail.com>
2026-04-15 13:27:51 +0700

    tess

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,28 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $students = Student::orderBy('name')->get();
         return view('students.index', compact('students'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('students.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -34,6 +26,7 @@ class StudentController extends Controller
             'phone'  => 'nullable|max:15',
             'school' => 'required|max:150',
         ]);
+
 
         Student::create([
             'name'   => $request->name,
